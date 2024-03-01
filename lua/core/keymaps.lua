3b8fb023a0f9b7041fe56c3b9c0359beb5b19da9
AllenWu233 <allenwu233@163.com>
2024-03-01 23:01:26 +0800

    2024/03/01-23:01

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -10,6 +10,8 @@ keymap.set("i", "jk", "<ESC>")
 
 
 -- ---------- 视觉模式 ---------- ---
+keymap.set("v", "jk", "<ESC>")
+
 -- 单行或多行移动
 keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 keymap.set("v", "K", ":m '<-2<CR>gv=gv")
