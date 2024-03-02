88951cbedd21163b4c26c263fc979c3e70d9585a
AllenWu233 <allenwu233@163.com>
2024-03-02 21:27:01 +0800

    2024/03/02-21:27

--- lua/plugins/plugins-setup.lua
+++ lua/plugins/plugins-setup.lua
@@ -57,7 +57,21 @@ local plugins = {
         end
     },
 
-    'norcalli/nvim-colorizer.lua'  -- color highlight
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        init = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+        end,
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        }
+    },
+
+    'norcalli/nvim-colorizer.lua', -- color highlight
 }
 
 local opts = {} -- 注意要定义这个变量
